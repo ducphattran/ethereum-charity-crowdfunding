94b23bc70181cbb184fa7407692b829f59a7bb9a
phatductran <phat.tranducvn@gmail.com>
2018-11-22 20:47:41 +0700

    Transfer code to an LogIn obejct

--- src/js/log-in.js
+++ src/js/log-in.js
@@ -1,22 +1,56 @@
-window.onload = function () {
-    
-    document.getElementById("btn-log-in").addEventListener('click', function (event) {
+LogIn = {
+    web3Provider: null,
+    contracts: {},
+
+    init: function () {
+        
+        return LogIn.initWeb3();
+    },
+
+    initWeb3: function () {
+        // Is there an injected web3 instance?
+        if (typeof web3 !== 'undefined') {
+            LogIn.web3Provider = web3.currentProvider;
+        } else {
+            // If no injected web3 instance is detected, fall back to Ganache
+            LogIn.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        }
+        web3 = new Web3(LogIn.web3Provider);
+
+        return LogIn.initContract();
+    },
+
+    initContract: function () {
+        $.getJSON('Funding.json', function (data) {
+            // Get the necessary contract artifact file and instantiate it with truffle-contract
+            var FundingArtifact = data;
+            LogIn.contracts.Funding = TruffleContract(FundingArtifact);
+
+            // Set the provider for our contract
+            LogIn.contracts.Funding.setProvider(LogIn.web3Provider);
+
+            // return LogIn.showCampaigns();
+        });
+
+        return LogIn.bindEvents();
+    },
+
+    bindEvents: function () {
+        $(document).on('click', '#btn-log-in', LogIn.checkAuth);
+    },
+
+    checkAuth: function (event) {
         event.preventDefault();
-        //input
-        var inputUsername = document.getElementById("username").value;
-        var inputPassword = document.getElementById("pwd").value;
-        inputPassword = web3.sha3(inputPassword);
-
-        checkAuth(inputUsername, inputPassword);
-        getProfile(inputUsername, inputPassword);
-    });
-
-     function checkAuth(username, password)  {
+        //get input value
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("pwd").value;
+        password = web3.sha3(password);
+        //Convert to Bytes
         username = window.App.hexToBytes(username, 16);
         password = window.App.hexToBytes(password, 32);
         var fundingInstance;
         //validate
-        window.App.contracts.Funding.deployed().then(function (instance) {
+        LogIn.contracts.Funding.deployed().then(function (instance) {
                 fundingInstance = instance;
                 return fundingInstance.checkAuth.call(username, password);
             }).then(function (result) {
@@ -25,7 +59,7 @@ window.onload = function () {
                         // Store
                         sessionStorage.setItem("username", username);
                         sessionStorage.setItem("password", password);
-                        window.location.href = "/profile.html";
+                        
                     } else {
                         document.write("Sorry, your browser does not support Web Storage...");
                     }
@@ -39,14 +73,12 @@ window.onload = function () {
             .catch(function (err) {
                 console.log(err);
             });
-    }
+        return LogIn.saveToSession(username,password);    
+    },
 
-    function getProfile(username, password){
-
-        username = window.App.hexToBytes(username, 16);
-        password = window.App.hexToBytes(password, 32);
-        //validate
-        window.App.contracts.Funding.deployed().then(function (instance) {
+    saveToSession: function (username,password) {
+        //get profile
+        LogIn.contracts.Funding.deployed().then(function (instance) {
                 fundingInstance = instance;
                 return fundingInstance.getUserProfile.call(username, password);
             }).then(function (result) {
@@ -59,6 +91,13 @@ window.onload = function () {
             .catch(function (err) {
                 console.log(err);
             });
+        return  window.location.href = "/profile.html";
     }
 
 };
+
+$(function () {
+    $(window).load(function () {
+        LogIn.init();
+    });
+});
